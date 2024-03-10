fe9ded76e272eb760cfa4eb459af549e0d530365
Sibongiseni Ndebele <43204005+Sbongslany@users.noreply.github.com>
2024-03-10 18:53:46 +0200

    Sign up and Login - fully integrated with firebase

--- lib/app.dart
+++ lib/app.dart
@@ -1,19 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:instax/blocs/authentication_bloc/authentication_bloc.dart';
+import 'package:user_repository/user_repository.dart';
+import 'app_view.dart';
 
 class MianApp extends StatelessWidget {
-  const MianApp({super.key});
+  final UserRepository userRepository;
+  const MianApp(this.userRepository, {super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: const Scaffold(
-          body: Center(child: Text('Hello World!!')),
-        ));
+    return MultiRepositoryProvider(
+        providers: [
+          RepositoryProvider<AuthenticationBloc>(
+              create: (_) => AuthenticationBloc(
+              myUserRepository: userRepository))
+        ],
+        child: const MyAppView()
+    );
   }
 }
